8f026090da8f71520aecc6b9b6527c410052c548
vaddula <vikramsra1616@gmail.com>
2025-02-02 16:16:53 -0500

    Initial Commit

---
+++ SimpleIosApp/SimpleIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleIosAppApp.swift
+//  SimpleIosApp
+//
+//  Created by Vijay Raghavan on 02/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
